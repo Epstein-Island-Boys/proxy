683b1f2beaa051cce3027d256d873f7bd1aa8eaa
tofu <redtofusea@gmail.com>
2026-03-23 22:26:25 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+  target: 'https://www.desmos.com',
+  changeOrigin: true,
+  onProxyRes: (proxyRes) => {
+    delete proxyRes.headers['x-frame-options'];
+    delete proxyRes.headers['content-security-policy'];
+  }
+}));
+
+app.listen(3000, () => {
+  console.log('Running on http://localhost:3000');
+});
