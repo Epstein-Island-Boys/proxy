36ba34b3eeac10a493140dc4912a7684600382c1
Epstein-Island-Boys <smurfcat21monday@gmail.com>
2026-03-23 22:49:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,43 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/', createProxyMiddleware({
-  target: 'https://www.google.com',
-  changeOrigin: true,
-  onProxyRes: (proxyRes) => {
-    delete proxyRes.headers['x-frame-options'];
-    delete proxyRes.headers['content-security-policy'];
+// Homepage with input box
+app.get('/', (req, res) => {
+  res.send(`
+    <h2>Simple Proxy</h2>
+    <form method="GET" action="/proxy">
+      <input type="text" name="url" placeholder="https://example.com" style="width:300px" required />
+      <button type="submit">Go</button>
+    </form>
+  `);
+});
+
+// Proxy route
+app.use('/proxy', (req, res, next) => {
+  let target = req.query.url;
+
+  if (!target) {
+    return res.send('No URL provided');
   }
-}));
+
+  // Add https:// if missing
+  if (!target.startsWith('http')) {
+    target = 'https://' + target;
+  }
+
+  return createProxyMiddleware({
+    target: target,
+    changeOrigin: true,
+    followRedirects: true,
+    pathRewrite: {
+      '^/proxy': '',
+    },
+    onProxyRes: function (proxyRes) {
+      delete proxyRes.headers['x-frame-options'];
+      delete proxyRes.headers['content-security-policy'];
+    }
+  })(req, res, next);
+});
 
 app.listen(3000, () => {
   console.log('Running on http://localhost:3000');
